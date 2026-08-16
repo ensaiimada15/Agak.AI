0e4965f2c7bb53bd410fbe57dbc41de6918b9efb
FranzylNatsuki <franzylbjornlmacalua@su.edu.ph>
2026-08-16 23:54:27 +0800

    Auth done

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -21,8 +21,8 @@ class Profile {
     return Profile(
       name: json['name'] as String? ?? '',
       email: json['email'] as String? ?? '',
-      seniorId: json['seniorId'] as String? ?? '',
-      mobileNo: json['mobileNo'] as String? ?? '',
+      seniorId: (json['senior_id'] ?? json['seniorId']) as String? ?? '',
+      mobileNo: (json['mobile_no'] ?? json['mobileNo']) as String? ?? '',
       age: json['age'] as int? ?? 0,
       birthday: json['birthday'] as String? ?? '',
       address: json['address'] as String? ?? '',
